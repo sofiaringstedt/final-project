9b01577e2c4d5a59c45a81506926ec79286475b0
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-05-30 16:10:01 +0200

    Validate email

--- server/models/user.js
+++ server/models/user.js
@@ -1,23 +1,36 @@
 import mongoose from "mongoose";
 import crypto from "crypto";
 
+const validateEmail = (email) => {
+  const emailValidator = /^\S+@\S+\.\S{2,}$/g;
+  return emailValidator.test(email);
+};
+
 const UserSchema = mongoose.Schema({
   email: {
     type: String,
-    required: true,
-    unique: true
+    unique: true,
+    trim: true,
+    validate: {
+      validator: (email) => validateEmail(email),
+      message: "Email validation failed"
+    },
+    required: [true, "Email is required"]
   },
   firstName: {
     type: String, 
-    required: true
+    trim: true,
+    required: [true, "First name is required"]
   },
   lastName: {
     type: String, 
-    required: true
+    trim: true,
+    required: [true, "Last name is required"]
   },
   password: {
     type: String, 
-    required: true
+    minLength: 8,
+    required: [true, "Password is required"]
   },
   accessToken: {
     type: String,
