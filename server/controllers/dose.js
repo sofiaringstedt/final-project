3ca8b88e94ff33e9b458b98dc3b5d933f61398da
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-06-14 19:56:36 +0200

    Add delete dose route

--- server/controllers/dose.js
+++ server/controllers/dose.js
@@ -1,5 +1,21 @@
 import Dose from "../models/dose.js";
 
+export const displayDose = async (req, res) => {
+  const userDose = await Dose.find();
+
+  try {
+    res.status(200).json({
+      success: true,
+      response: userDose
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      response: error
+    })
+  };
+};
+
 export const createDose = async (req, res) => {
   const { dose, date, batchNumber, nextDose } = req.body;
 
@@ -33,21 +49,25 @@ export const createDose = async (req, res) => {
       success: false,
       response: error
     })
-  }
+  };
 };
 
-export const displayDose = async (req, res) => {
-  const userDose = await Dose.find();
+export const deleteDose = async (req, res) => {
+  const { doseId } = req.params;
 
-   try {
-    res.status(200).json({
-      success: true,
-      response: userDose
-    });
-  } catch(error) {
+  try {
+    const deletedDose = await Dose.findOneAndDelete({ _id: doseId });
+
+    if (deletedDose) {
+      res.status(200).json({ success: true, response: deletedDose });
+    } else {
+      res.status(404).json({ success: false, response: "Dose not found" });
+    }
+  } catch (error) {
     res.status(400).json({
       success: false,
+      message: "Could not delete dose",
       response: error
-    })
+    });
   };
-}
+};
