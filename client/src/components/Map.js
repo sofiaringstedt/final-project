d55f0c7ea0a8a6f1cfae4f031886ab678d0f0b22
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-06-16 10:05:00 +0200

    Deleting unused files

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -4,7 +4,7 @@ import areas from "../data/areas.json"
 
 const Map = () => {
  return (
-  <MapContainer center={[59, 15]} zoom={6} scrollWheelZoom={true}>
+  <MapContainer center={[62, 15]} zoom={5} scrollWheelZoom={true}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
