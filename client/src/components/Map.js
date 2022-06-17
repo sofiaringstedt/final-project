fb0183245952f3d88414c84b382de35672be1e2b
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-06-17 16:43:24 +0200

    Refactor map area code

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -7,30 +7,23 @@ const Map = () => {
   const filteredMidRiskAreas = areas.filter(midRiskAreas => midRiskAreas.riskLevel === "mid");
   const filteredHighRiskAreas = areas.filter(highRiskAreas => highRiskAreas.riskLevel === "high");
 
+  const mapRiskAreas = (filteredRiskAreas, dotColor) => {
+   return filteredRiskAreas.map((mapArea) => {
+    const positions = [mapArea.lat, mapArea.lng]
+    const cityName = mapArea.city
+   return <Circle key={mapArea.city} center={positions} radius={25000} color={dotColor}><Popup>{cityName}</Popup></Circle>
+  })
+  }
+
   return (
     <MapContainer center={[63, 16]} zoom={5} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-
-      {filteredLowRiskAreas.map((area) => {
-        const positions = [area.lat, area.lng]
-        const cityName = area.city
-        return <Circle key={area.city} center={positions} radius={25000} color="yellow"><Popup>{cityName}</Popup></Circle>
-      })}
-
-      {filteredMidRiskAreas.map((area) => {
-        const positions = [area.lat, area.lng]
-        const cityName = area.city
-        return <Circle key={area.city} center={positions} radius={25000} color="orange"><Popup>{cityName}</Popup></Circle>
-      })}
-
-      {filteredHighRiskAreas.map((area) => {
-        const positions = [area.lat, area.lng]
-        const cityName = area.city
-        return <Circle key={area.city} center={positions} radius={25000} color="red"><Popup>{cityName}</Popup></Circle>
-      })}
+      {filteredLowRiskAreas && mapRiskAreas(filteredLowRiskAreas, "yellow")}
+      {filteredMidRiskAreas && mapRiskAreas(filteredMidRiskAreas, "orange")}
+      {filteredHighRiskAreas && mapRiskAreas(filteredHighRiskAreas, "red")}
     </MapContainer>
   );
 };
