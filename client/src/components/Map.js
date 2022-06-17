749f2db98081a56c57ed7e20be9defe30ec5fbce
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-06-17 15:31:50 +0200

    Refactored and added data to map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -3,7 +3,8 @@ import { MapContainer, TileLayer, Circle, Popup } from "react-leaflet";
 import areas from "../data/mapAreas.json"
 
 const Map = () => {
-  const filteredHighRiskAreas = areas.filter(highRiskAreas => highRiskAreas.highRisk === true);
+  const filteredHighRiskAreas = areas.filter(highRiskAreas => highRiskAreas.riskLevel === "high");
+  const filteredLowRiskAreas = areas.filter(highRiskAreas => highRiskAreas.riskLevel === "low");
 
   return (
     <MapContainer center={[63, 16]} zoom={5} scrollWheelZoom={true}>
@@ -13,15 +14,21 @@ const Map = () => {
       />
 
       {areas.map((area) => {
-        const positions = [area.coordinates.lat, area.coordinates.long]
-        const cityName = area.cityName
-        return <Circle key={area.id} center={positions} radius={30000} color="orange"><Popup>{cityName}</Popup></Circle>
+        const positions = [area.lat, area.lng]
+        const cityName = area.city
+        return <Circle key={area.city} center={positions} radius={25000} color="orange"><Popup>{cityName}</Popup></Circle>
       })}
 
       {filteredHighRiskAreas.map((area) => {
-        const positions = [area.coordinates.lat, area.coordinates.long]
+        const positions = [area.lat, area.lng]
         const cityName = area.cityName
-        return <Circle key={area.id} center={positions} radius={30000} color="red"><Popup>{cityName}</Popup></Circle>
+        return <Circle key={area.city} center={positions} radius={25000} color="red"><Popup>{cityName}</Popup></Circle>
+      })}
+
+      {filteredLowRiskAreas.map((area) => {
+        const positions = [area.lat, area.lng]
+        const cityName = area.cityName
+        return <Circle key={area.city} center={positions} radius={25000} color="yellow"><Popup>{cityName}</Popup></Circle>
       })}
     </MapContainer>
   );
