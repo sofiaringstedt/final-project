1f74382b1557e8b9e3476e634dfcda8c497fe744
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-06-17 16:20:18 +0200

    Filter out depending on low mid high risk

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -3,8 +3,9 @@ import { MapContainer, TileLayer, Circle, Popup } from "react-leaflet";
 import areas from "../data/mapAreas.json"
 
 const Map = () => {
+  const filteredLowRiskAreas = areas.filter(lowRiskAreas => lowRiskAreas.riskLevel === "low");
+  const filteredMidRiskAreas = areas.filter(midRiskAreas => midRiskAreas.riskLevel === "mid");
   const filteredHighRiskAreas = areas.filter(highRiskAreas => highRiskAreas.riskLevel === "high");
-  const filteredLowRiskAreas = areas.filter(highRiskAreas => highRiskAreas.riskLevel === "low");
 
   return (
     <MapContainer center={[63, 16]} zoom={5} scrollWheelZoom={true}>
@@ -13,7 +14,13 @@ const Map = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      {areas.map((area) => {
+      {filteredLowRiskAreas.map((area) => {
+        const positions = [area.lat, area.lng]
+        const cityName = area.city
+        return <Circle key={area.city} center={positions} radius={25000} color="yellow"><Popup>{cityName}</Popup></Circle>
+      })}
+
+      {filteredMidRiskAreas.map((area) => {
         const positions = [area.lat, area.lng]
         const cityName = area.city
         return <Circle key={area.city} center={positions} radius={25000} color="orange"><Popup>{cityName}</Popup></Circle>
@@ -21,14 +28,8 @@ const Map = () => {
 
       {filteredHighRiskAreas.map((area) => {
         const positions = [area.lat, area.lng]
-        const cityName = area.cityName
+        const cityName = area.city
         return <Circle key={area.city} center={positions} radius={25000} color="red"><Popup>{cityName}</Popup></Circle>
-      })}
-
-      {filteredLowRiskAreas.map((area) => {
-        const positions = [area.lat, area.lng]
-        const cityName = area.cityName
-        return <Circle key={area.city} center={positions} radius={25000} color="yellow"><Popup>{cityName}</Popup></Circle>
       })}
     </MapContainer>
   );
