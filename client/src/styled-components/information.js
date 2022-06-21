c84e7462dcf54ed2f85b84cbd51fd7fdb2331ae5
frida axelsson <axelsson.frida@gmail.com>
2022-06-21 22:16:38 +0200

    styling and added content and images

--- client/src/styled-components/information.js
+++ client/src/styled-components/information.js
@@ -9,7 +9,6 @@ export const InformationContainer = styled.div`
 width: 100%;
 `;
 export const InformationWrapper = styled.div`
-/* width: 80%; */
 display: flex;
 flex-direction: column;
 align-items: flex-start;
@@ -20,29 +19,23 @@ margin-top: 18vh;
 margin-bottom: 20vh;
 `;
 export const InfoHeadingOne = styled.h1`
-/* font-size: 3rem; */
 color: #175C4C;
 font-size: 42px;
 `;
 export const InfoHeadingTwo = styled.h2`
-/* font-size: 1.5rem; */
 color: #175C4C;
 font-size:25px;
 `;
 export const InfoHeadingThree = styled.h3`
-/* font-size: 1.25rem; */
 font-weight: 600;
 font-size: 20px;
 `;
 export const InfoHeadingFour = styled.h4`
     font-weight: 300;
-    /* font-size: 25px; */
-    /* font-size: 1.375rem; */
     font-size: 22px;
 `;
 export const InfoParagraph = styled.p`
     font-weight: 300;
-    /* font-size: 1rem; */
     font-size: 20px;
     line-height: 27px;
     line-break: loose;
